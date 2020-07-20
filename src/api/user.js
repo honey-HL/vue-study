5a3f95c0b42ad4506cc81ce068825307a33474c0
honney <honneysliane@126.com>
2020-07-20 14:08:02 +0800

    [empty] initial commit

--- src/api/user.js
+++
@@ -1,9 +0,0 @@
-import axios from '@/utils/request'
-
-export function login(data) {
-  return axios.post('/user/login', data)
-}
-
-export function getInfo() {
-  return axios.get('/user/info')
-}
--- src/icons/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import SvgIcon from '@/components/SvgIcon.vue'
-
-const req = require.context('./svg', false, /\.svg$/)
-req.keys().map(req);
-
-Vue.component('svg-icon', SvgIcon)
